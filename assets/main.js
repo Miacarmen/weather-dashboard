acd02a3782b4a42ae98b328d9b20070856fa40b3
Miacarmen <maitreyacarmen@gmail.com>
2022-04-08 22:36:09 -0700

    added bootstrap styles and js querySelectors

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,8 @@
+var searchCity = document.querySelector('.form-control');
+var searchBtn = document.querySelector('.btn-primary');
+console.log(searchBtn);
+// get current location
 
+// get current date
 
-
-
-
-
-
-
-
-
-
-
+// get temperature
