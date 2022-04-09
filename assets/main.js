c2bbc7cc5af086e7764d35646cc32062f789b6c5
Miacarmen <maitreyacarmen@gmail.com>
2022-04-08 22:51:08 -0700

    added JS pseudocode and primary querySelectors

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,29 @@
-var searchCity = document.querySelector('.form-control');
+var citySearch = document.querySelector('.searchCity');
 var searchBtn = document.querySelector('.btn-primary');
-console.log(searchBtn);
-// get current location
+var input = document.querySelectorAll('.form-control');
+var cityList = document.querySelectorAll('.list-group');
+var cityName = document.querySelectorAll('.list-group-item');
+console.log(cityName);
 
 // get current date
+// moment.js
 
-// get temperature
+// get current location
+// get temperature for current location
+// get humidity for current location
+// get wind speed for current location
+// get UV index for current location
+// API requests
+
+
+
+
+
+
+
+// onClick of searchBtn add new button to top of list-group displaying that city name
+// API request weather for searched city
+// update col-sm-7 city-stats to those of the input city
+// update 5 day forcast blocks for that city
+
+// onClick of each list-group item, display clicked in city-stats
